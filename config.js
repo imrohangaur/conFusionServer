3ed3ef50364f69b42164315099502cf89541630d
Rohan Gaur <imrohangaur@gmail.com>
2020-10-02 19:24:57 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId : '977875459348282',
+        clientSecret : 'b9005cd1890933a091d7512cd7ed6510'
+    }
 }
