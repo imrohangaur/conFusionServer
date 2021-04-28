b03eb8cad790036164e8b02ca4d89164bd171174
imrohangaur <32222938+imrohangaur@users.noreply.github.com>
2021-04-28 16:04:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
+    'secretKey': '12345-67890-09876-54641',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion',
     'facebook' : {
-        clientId : '977875459348282',
-        clientSecret : 'b9005cd1890933a091d7512cd7ed6510'
+        clientId : '9778754593282',
+        clientSecret : 'b9005cd1890933a0d7512cd7ed6510'
     }
 }
